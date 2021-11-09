0d1254a9ac038e2935e2ca83084cc177cda948ab
SivaPrasad004 <sivaprasad.reddy@adqservices.com>
2021-11-09 16:04:53 +0530

    first

---
+++ sample_script.py
@@ -0,0 +1,11 @@
+import time
+
+print("Message-1", flush=True)
+time.sleep(1)
+print("Message-2",flush=True)
+time.sleep(1)
+print("Message-3", flush=True)
+time.sleep(1)
+print("Message-4", flush=True)
+time.sleep(1)
+print("Message-5", flush=True)
